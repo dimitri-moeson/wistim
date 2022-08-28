20be46a6916701f22bf2030a7c484d08accc7925
MOESON <d.moeson@gmail.com>
2022-08-28 17:48:32 +0200

    mvc i18n remove

--- config/modules.config.php
+++ config/modules.config.php
@@ -12,7 +12,6 @@ return [
     'Laminas\Mvc\Plugin\Prg',
     'Laminas\Session',
     'Laminas\Mvc\Middleware',
-    'Laminas\Mvc\I18n',
     'Laminas\Form',
     'Laminas\I18n',
     'Laminas\Log',
